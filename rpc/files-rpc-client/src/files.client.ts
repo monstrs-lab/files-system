df22e7f395671af8820ee397d431c458bdbf1568
Andrey Linko <AndreyLinko@gmail.com>
2023-09-11 01:02:58 +0300

    feat: init rpc client

---
+++ rpc/files-rpc-client/src/files.client.ts
@@ -0,0 +1,18 @@
+import type { PromiseClient }  from '@connectrpc/connect'
+
+import { createPromiseClient } from '@connectrpc/connect'
+import { createGrpcTransport } from '@connectrpc/connect-node'
+
+import { FilesService }        from '@files-system/files-rpc/connect'
+
+export const createFilesClient = (options = {}): PromiseClient<typeof FilesService> =>
+  createPromiseClient(
+    FilesService,
+    createGrpcTransport({
+      httpVersion: '2',
+      baseUrl: process.env.PUBLICATIONS_SERVICE_URL || 'http://0.0.0.0:50051',
+      ...options,
+    })
+  )
+
+export const files = createFilesClient()
---
+++ rpc/files-rpc-client/src/index.ts
@@ -0,0 +1,4 @@
+export * from '@files-system/files-rpc/interfaces'
+export * from '@bufbuild/protobuf'
+
+export * from './files.client.js'
