ce3dbb372de8546efae22ea24144b490c756ff52
Andrey Linko <AndreyLinko@gmail.com>
2023-09-11 10:35:17 +0300

    feat: change rpc paths

--- rpc/files-rpc/src/gen/connect/index.ts
+++ rpc/files-rpc/src/gen/connect/index.ts
@@ -1,2 +1,2 @@
-export * from './tech/monstrs/filessystem/files/v1alpha1/files.service_connect.js'
-export * from './tech/monstrs/filessystem/files/v1alpha1/files.service_pb.js'
+export * from './tech/monstrs/files_system/v1alpha1/files.service_connect.js'
+export * from './tech/monstrs/files_system/v1alpha1/files.service_pb.js'
